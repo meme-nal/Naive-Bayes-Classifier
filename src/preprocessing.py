64bebe97319d8dc7f58c071f76b245c30a9aecc1
me_me <ichshlanger2@gmail.com>
2024-03-24 21:45:44 +0300

    update

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,4 +6,4 @@ data = data.drop(columns='Id')
 species = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
 data = data.replace(to_replace=species, value=(1,2,3))
 
-data.to_csv("./data/preprocessed/Iris_preprocessed.csv")
+data.to_csv("./data/preprocessed/Iris_preprocessed.csv", index=False)
