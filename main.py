ce107810739f3c9f0d7fcd29fa2b0b86cc5c2ddc
me_me <ichshlanger2@gmail.com>
2024-03-25 19:37:15 +0300

    added conversation continuous features to categorical

--- main.py
+++ main.py
@@ -10,18 +10,21 @@ dataset = dataset.sample(frac=1) # shuffling
 
 ### Simple EDA
 
+# check if there are missings in data
+print(dataset.isna().mean().sort_values(ascending=False))
+
 # check if predictable classes are unbalanced
 fig = plt.figure(figsize=(8,8))
 plt.hist(dataset['Species'])
 plt.title("count of labels")
-plt.savefig('./results/balanced_data.jpg')
+plt.savefig('./results/eda/balanced_data.jpg')
 
 # check correlation between features
 fig = plt.figure(figsize=(8,8))
 corr_matrix = dataset.iloc[:,:-1].corr(method='pearson')
-sns.heatmap(corr_matrix, annot=True)
+sns.heatmap(corr_matrix, annot=False)
 plt.title("Correlation matrix")
-plt.savefig('./results/correlations.jpg')
+plt.savefig('./results/eda/correlations.jpg')
 
 # check distributions of all features
 features = list(dataset.columns)[:-1]
@@ -29,12 +32,19 @@ for feature in features:
     fig = plt.figure(figsize=(8,8))
     plt.hist(dataset[feature])
     plt.title(feature)
-    plt.savefig('./results/'+str(feature)+'.jpg')
+    plt.savefig('./results/eda/'+str(feature)+'.jpg')
 
+# convert continuous features to categorical features
+dataset['cat_SepalLengthCm'] = pd.cut(dataset['SepalLengthCm'].values, bins=4, labels=[0,1,2,3])
+dataset['cat_SepalWidthCm'] = pd.cut(dataset['SepalWidthCm'].values, bins=4, labels=[0,1,2,3])
+dataset['cat_PetalLengthCm'] = pd.cut(dataset['PetalLengthCm'].values, bins=4, labels=[0,1,2,3])
+dataset['cat_PetalWidthCm'] = pd.cut(dataset['PetalWidthCm'].values, bins=4, labels=[0,1,2,3])
 
+dataset = dataset.drop(columns=['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm'])
+dataset = dataset[['cat_SepalLengthCm', 'cat_SepalWidthCm', 'cat_PetalLengthCm', 'cat_PetalWidthCm', 'Species']]
 
 # splitting into train and test data
-#train_size = 0.8
+train_size = 0.8
 
 #train = dataset.iloc[:int(train_size*dataset.shape[0]),:]
 #test = dataset.iloc[int(train_size*dataset.shape[0]):,:]
